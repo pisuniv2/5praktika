de8897119757a48b759e97b9777dc42f90474b56
Jhony Boy <mdmarust@gmail.com>
2024-11-17 19:47:51 +0500

    Add files via upload

---
+++ RepoB/main.cpp
@@ -0,0 +1,14 @@
+// Эта программа является учебной. Создатель Холчев Никита
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a, b;
+    cout << "Введите первое число: ";
+    cin >> a;
+    cout << "Введите второе число: ";
+    cin >> b;
+    cout << "Разность чисел: " << a - b << endl;
+    return 0;
+}
